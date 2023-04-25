443a416cd69532436a160495d8445c1212bb1beb
NahuelIradis <nahueliradis5@gmail.com>
2023-04-25 15:11:33 -0300

    Add dark-theme

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ window.onscroll = function(){
         header.classList.remove('active');
     }
 } 
+
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
+
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    } else {
+        icon.src = "images/moon.png";
+    }
+}
